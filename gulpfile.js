6ced297a67009cb133fd9d9d6c0c77262b0c9ad3
s763397709 <763397709@qq.com>
2018-05-04 08:14:36 +0800

    three

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver')
+var data = require('src/data.json')
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 9090,
+            livereload: true,
+            open: true,
+            middleware: function(req, res, next) {
+                if (req.url === '/index') {
+                    res.end(JSON.stringify(data))
+                }
+                next();
+            }
+        }))
+})
+
+gulp.task('watch', function() {
+    gulp.watch('src/scss/*.scss')
+})
+
+gulp.task('default', ['server', 'watch']);
